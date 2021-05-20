1d37cb4976559f947fac625ad67dbfdd12ad1462
Michael <michael.spengler@sap.com>
2021-05-20 15:25:11 +0200

    add example web server code

---
+++ example-application/backend/example-deno-server.ts
@@ -0,0 +1,15 @@
+import { opine } from "https://deno.land/x/opine@1.3.4/mod.ts";
+import { Request } from 'https://deno.land/x/request@1.3.0/request.ts'
+
+const app = opine();
+
+app.get("/getISSPosition", async function (req, res) {
+
+    const result = await Request.get('http://api.open-notify.org/iss-now.json')
+
+    // console.log(result)
+
+    res.send(result);
+});
+
+app.listen(3000, () => console.log("server has started on http://localhost:3000 🚀"));
