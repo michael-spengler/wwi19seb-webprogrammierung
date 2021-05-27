a3a0e143d28fa825bb054c5792824624a118b2f9
Michael <michael.spengler@sap.com>
2021-05-27 11:48:14 +0200

    prepare demo to send an html file to the client

--- example-application/backend/example-deno-server.ts
+++ example-application/backend/example-deno-server.ts
@@ -1,3 +1,6 @@
+// to start this web server you can run:
+// deno run --allow-net --allow-read example-application/backend/example-deno-server.ts
+
 import { opine } from "https://deno.land/x/opine@1.3.4/mod.ts";
 import { Request } from 'https://deno.land/x/request@1.3.0/request.ts'
 
@@ -7,9 +10,17 @@ app.get("/getISSPosition", async function (req, res) {
 
     const result = await Request.get('http://api.open-notify.org/iss-now.json')
 
-    // console.log(result)
-
     res.send(result);
 });
+
+app.get("/", async function (req, res) {
+
+    const pathToIndexHTMLFile = `${Deno.cwd()}/example-application/frontend/index.html`
+
+    console.log(pathToIndexHTMLFile)
+
+    res.sendFile(pathToIndexHTMLFile);
+});
+
 
 app.listen(3036, () => console.log("server has started on http://localhost:3036 🚀"));
