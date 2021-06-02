3d231d2903c89cb790fd74a771bb67fab870c671
Michael <michael.spengler@sap.com>
2021-06-02 13:10:56 +0200

    prepare as example for hackathon topic

---
+++ example-vue-app/vueconfig.js
@@ -0,0 +1,24 @@
+module.exports = {
+    publicPath: "/example-vue-app/",
+    transpileDependencies: ["vuetify"],
+    pwa: {
+      themeColor: "#363636",
+      msTileColor: "#3F51B5",
+      manifestOptions: {
+        name: "Fancy Flashcard",
+        short_name: "Fancy Flashcard",
+      },
+    },
+    chainWebpack: (config) => {
+      config.plugin("html").tap((args) => {
+        args[0].title = "Fancy Flashcard";
+        return args;
+      });
+      config.plugin("define").tap((args) => {
+        args[0]["process.env"]["VERSION"] = JSON.stringify(
+          require("./package.json").version
+        );
+        return args;
+      });
+    },
+  };
